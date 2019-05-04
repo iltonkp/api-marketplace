565601a8386484ff50364793155c4b230d194d08
iltonkp <iltonk.si@gmail.com>
2019-05-03 23:16:14 -0300

    configurando sentry

--- src/server.js
+++ src/server.js
@@ -2,17 +2,24 @@ const express = require('express')
 const mongoose = require('mongoose')
 const validate = require('express-validation')
 const Youch = require('youch')
+const Sentry = require('@sentry/node')
 const databaseConfig = require('./config/database')
+const sentryConfig = require('./config/sentry')
 
 class App {
   constructor () {
     this.express = express()
     this.isDev = process.env.NODE_ENV !== 'production'
 
+    this.sentry()
     this.database()
     this.middlewares()
     this.routes()
     this.exception(this.isDev)
+  }
+
+  sentry () {
+    Sentry.init(sentryConfig)
   }
 
   database () {
@@ -24,6 +31,7 @@ class App {
 
   middlewares () {
     this.express.use(express.json())
+    this.express.use(Sentry.Handlers.requestHandler())
   }
 
   routes () {
@@ -31,12 +39,16 @@ class App {
   }
 
   exception (isDev) {
+    if (process.env.NODE_ENV === 'production') {
+      this.express.use(Sentry.Handlers.errorHandler())
+    }
+
     this.express.use(async (err, req, res, next) => {
       if (err instanceof validate.ValidationError) {
         return res.status(err.status).json(err)
       }
 
-      if (process.env.NODE_ENV !== 'production') {
+      if (isDev) {
         const youch = new Youch(err, req)
         return res.json(await youch.toJSON())
       }
