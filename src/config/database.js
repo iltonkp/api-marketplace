f0b77a29023efd343ae3b22dae4077a46c249ce3
iltonkp <iltonk.si@gmail.com>
2019-05-03 23:32:57 -0300

    Configurando variáveis de ambiente com dotenv

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,3 @@
 module.exports = {
-  uri:
-    'mongodb+srv://docker:docker@cluster0-kuomn.azure.mongodb.net/marketplace?retryWrites=true'
+  uri: process.env.DB_URL
 }
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 module.exports = {
-  dsn: 'https://49c8a168e87c4a1abac41f2b58351284@sentry.io/1452271'
+  dsn: process.env.SENTRY_DSN
 }
